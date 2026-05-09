3e4941451c11b85cb7b944382746d9165b33a7bb
prprem4327-alt <prprem4327@gmail.com>
2026-05-09 22:50:41 +0530

    a1

--- LAB-01/src/Activity-01.cpp
+++ LAB-01/src/Activity-01.cpp
@@ -1,1 +1,25 @@
-//line one
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    // Configure PB0 as output
+    DDRB = DDRB | (1 << PB0);
+
+    while (1)
+    {
+        // Turn LED ON
+        PORTB = PORTB | (1 << PB0);
+
+        _delay_ms(1000);
+
+        // Turn LED OFF
+        PORTB = PORTB & ~(1 << PB0);
+
+        _delay_ms(1000);
+    }
+
+    return 0;
+}
