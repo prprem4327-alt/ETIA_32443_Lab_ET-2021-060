9f882ab303e79855c6279bee68ec5ebdfe3c19b9
prprem4327-alt <prprem4327@gmail.com>
2026-05-09 22:56:29 +0530

    a1

--- LAB-01/src/Activity-01.cpp
+++ LAB-01/src/Activity-01.cpp
@@ -5,17 +5,14 @@
 
 int main(void)
 {
-    // Configure PB0 as output
     DDRB = DDRB | (1 << PB0);
 
     while (1)
     {
-        // Turn LED ON
         PORTB = PORTB | (1 << PB0);
 
         _delay_ms(1000);
 
-        // Turn LED OFF
         PORTB = PORTB & ~(1 << PB0);
 
         _delay_ms(1000);
