e9c8ebc3037238d8f2c4946803d20c45e8f521bd
prprem4327-alt <prprem4327@gmail.com>
2026-05-09 22:59:23 +0530

    a2

---
+++ LAB-01/src/Activity-02.cpp
@@ -0,0 +1,17 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    DDRC |= 0x0F;   
+
+    while(1)
+    {
+        PORTC = 0x0F;  
+        _delay_ms(500);
+
+        PORTC = 0x00;  
+        _delay_ms(500);
+    }
+}
