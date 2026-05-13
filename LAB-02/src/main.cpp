fc5d65c27e3aec2152e0e0b13e084c3830ee3c35
prprem4327-alt <prprem4327@gmail.com>
2026-05-13 11:40:56 +0530

    pir

--- LAB-02/src/main.cpp
+++ LAB-02/src/main.cpp
@@ -1,18 +1,23 @@
 #include <Arduino.h>
+#include <avr/io.h>
+#include <util/delay.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int main(void)
+{
+    DDRB |= (1 << PB5);     // LED output
+    DDRD &= ~(1 << PD2);    // PIR input
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+    while (1)
+    {
+        if (PIND & (1 << PD2))
+        {
+            PORTB |= (1 << PB5);   // LED ON
+        }
+        else
+        {
+            PORTB &= ~(1 << PB5);  // LED OFF
+        }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+        _delay_ms(100);
+    }
 }
